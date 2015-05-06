3831fe911c3f1d71722900c72060995a4e7b88db
zoranac <michael.pattengale@T3600-RIKER.iam.local>
2015-05-06 18:28:46 -0500

    Working on Ranged Enemy

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -20,6 +20,7 @@ public class EnemyScript : MonoBehaviour {
 	public bool HitLeft;
 
 	public GameObject target;
+	public Vector3 targetPos = Vector3.zero;
     public GameObject layout;
     public float damage;
 	bool stuck = false;
@@ -27,10 +28,15 @@ public class EnemyScript : MonoBehaviour {
 	public float speedMin = .001f; //ADDED
 	public bool slowed = false; //ADDED
 	float speed = .02f;
+	public bool Ranged = false;
+	public float ShotInterval = 0;
+	float shotTime = 0;
+	public GameObject RangedShotPrefab;
 	enum State
 	{
 		Idle,
 		Moving,
+		Wander,
 		Attacking
 	}
 
@@ -55,9 +61,22 @@ public class EnemyScript : MonoBehaviour {
 		{
 			if (obj.tag == "Player")
 			{
-				playerInSightRange = true;
-				target = obj.gameObject;
-				speed = Random.Range((obj.transform.position - transform.position).magnitude*speedMin,(obj.transform.position - transform.position).magnitude*speedMax); //CHANGED
+				bool wallHit = false;
+				foreach (RaycastHit2D obj2 in Physics2D.LinecastAll(transform.position,obj.transform.position))
+				{
+					if (obj2.collider.tag == "Wall")
+					{
+						wallHit = true;
+						break;
+					}
+				}
+				if (!wallHit)
+				{
+					playerInSightRange = true;
+					target = obj.gameObject;
+					targetPos = target.transform.position;
+					speed = Random.Range((obj.transform.position - transform.position).magnitude*speedMin,(obj.transform.position - transform.position).magnitude*speedMax); //CHANGED
+				}
 			}
 
 		}
@@ -67,20 +86,56 @@ public class EnemyScript : MonoBehaviour {
 			{
 				playerInAttackRange = true;
 				target = obj.gameObject;
+				targetPos = target.transform.position;
 			}
 
 		}
 
-		foreach (Collider2D obj in Physics2D.OverlapCircleAll(transform.position,CollisionSpacerRange))
+		foreach (Collider2D obj in Physics2D.OverlapCircleAll(transform.position,AttackRange/1.5f))
 		{
-			if (obj.tag == "Enemy"&& obj.gameObject != gameObject || obj.tag == "Player")
+			if (obj.tag == "Player")
 			{
-				transform.position = Vector3.MoveTowards(transform.position,-(obj.transform.position)*2, speed);
+				//transform.position = Vector3.MoveTowards(transform.position,-(obj.transform.position)*4, speed);
 				overlappingEnemy = true;
-				//rigidbody2D.AddForce();
+				break;
 			}
 		}
+		if (!overlappingEnemy)
+		{
+			foreach (Collider2D obj in Physics2D.OverlapCircleAll(transform.position,CollisionSpacerRange))
+			{
+				if (obj.tag == "Enemy"&& obj.gameObject != gameObject || obj.tag == "Player")
+				{
+					targetPos = -(obj.transform.position)*2;
+					if (transform.position.x > obj.transform.position.x)
+					{
+						targetPos.x = transform.position.x + obj.transform.position.x;
+					}
+					else
+					{
+						targetPos.x = transform.position.x - obj.transform.position.x;
+					}
 
+					if (transform.position.y > obj.transform.position.y)
+					{
+						targetPos.y = transform.position.y + obj.transform.position.y;
+					}
+					else
+					{
+						targetPos.y = transform.position.y - obj.transform.position.y;
+					}
+					transform.position = Vector3.MoveTowards(transform.position,targetPos*2, speed);
+					overlappingEnemy = true;
+					//rigidbody2D.AddForce();
+					break;
+				}
+			}
+		}
+//		if (overlappingEnemy)
+//		{
+//			//currentState = State.Idle;
+//		}
+//		else 
 		if (playerInAttackRange)
 		{
 			currentState = State.Attacking;
@@ -93,7 +148,7 @@ public class EnemyScript : MonoBehaviour {
 		}
 		else
 		{
-			currentState = State.Idle;
+			currentState = State.Wander;
 			target = null;
 		}
 
@@ -106,10 +161,13 @@ public class EnemyScript : MonoBehaviour {
 		{
 			move(target);
 		}
-		
+		if (currentState == State.Wander)
+		{
+			Wander();
+		}
 		if (currentState == State.Idle)
 		{
-			
+
 		}
 		Vector3 pos = transform.position;
 		pos.z = 0;
@@ -123,9 +181,20 @@ public class EnemyScript : MonoBehaviour {
 		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - 90;
 		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 		rigidbody2D.velocity = Vector2.zero;
-
-        target.gameObject.GetComponent<PlayerHealthScript>().Decriment(damage);
-        layout.gameObject.GetComponent<HealthBarFlash>().FlashBar();
+		if (!Ranged)
+		{
+      	  	target.gameObject.GetComponent<PlayerHealthScript>().Decriment(damage);
+        	layout.gameObject.GetComponent<HealthBarFlash>().FlashBar();
+		}
+		else
+		{
+			if (Time.time >= shotTime + ShotInterval)
+			{
+				GameObject temp = (GameObject)Instantiate(RangedShotPrefab,transform.position,transform.rotation);
+				temp.GetComponent<EnemyShotScript>().SetDamage(damage);
+				shotTime = Time.time;
+			}
+		}
 	}
 	void move(GameObject moveTarget)
 	{
@@ -151,6 +220,24 @@ public class EnemyScript : MonoBehaviour {
 
 				transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
 			}
+		}
+	}
+	void Wander()
+	{
+		if (moveAround())
+		{
+			//transform.position = Vector3.MoveTowards(transform.position, targetPos, .03f);
+		}
+		else
+		{
+			//hit = false;
+			targetPos = targetPos.normalized;
+			targetPos.x = Random.Range(targetPos.x - .25f,targetPos.x + .25f);
+			targetPos.y = Random.Range(targetPos.y - .25f,targetPos.y + .25f);
+			targetPos *= 1000;
+			targetPos.z = -1;
+			
+			transform.position = Vector3.MoveTowards(transform.position, targetPos, speed*3);
 		}
 	}
 	bool moveAround()
---
+++ Assets/Scripts/EnemyShotScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShotScript : MonoBehaviour {
+	public float Speed;
+	float damage;
+	// Use this for initialization
+	void Start () {
+		rigidbody2D.velocity = -(transform.position - GameObject.Find ("Player").transform.position).normalized *Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (Collider2D obj in Physics2D.OverlapPointAll(transform.position))
+		{
+			if (obj.tag == "Wall")
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+	public void SetDamage(float d)
+	{
+		damage = d;
+	}
+	void OnTriggerEnter2D(Collider2D otherObj)
+	{
+		if (otherObj.tag == "Player")
+		{
+			otherObj.gameObject.GetComponent<PlayerHealthScript>().Decriment(damage);
+			GameObject.Find("Health").gameObject.GetComponent<HealthBarFlash>().FlashBar();
+			Destroy(gameObject);
+		}
+	}
+
+}
